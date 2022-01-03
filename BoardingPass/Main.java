4f66142e40fa098e81d32914bec10bca3a0232fe
ashucoding <55626983+ashucoding@users.noreply.github.com>
2022-01-02 22:10:46 -0500

    Add files via upload

---
+++ BoardingPass/Main.java
@@ -0,0 +1,23 @@
+package com.example.boardingpass;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = new FXMLLoader.load(getClass().getResource("boardingPass.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
